c6a9558a2906a8a492934c7f6ad6e545c835670b
Afdhal D <80806469+afdhald23@users.noreply.github.com>
2022-01-17 19:25:24 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
-// window.onload = () => {
-//   'use strict';
+window.onload = () => {
+  'use strict';
 
-//   if ('serviceWorker' in navigator) {
-//     navigator.serviceWorker
-//              .register('./sw.js');
-//   }
-// }
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function () {
-    navigator.serviceWorker.register('./sw.js').then(function (registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function (err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+             .register('./sw.js');
+  }
 }
+
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', function () {
+//     navigator.serviceWorker.register('./sw.js').then(function (registration) {
+//       // Registration was successful
+//       console.log('ServiceWorker registration successful with scope: ', registration.scope);
+//     }, function (err) {
+//       // registration failed :(
+//       console.log('ServiceWorker registration failed: ', err);
+//     });
+//   });
+// }
